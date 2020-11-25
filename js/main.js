634f83a770b9c6f424e31403b28d7d893b2cf2c1
Sokol2020 <72626933+Sokol2020@users.noreply.github.com>
2020-11-25 16:02:23 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
+})
+
+var mySwiper = new Swiper('.swiper-container', {
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
+
